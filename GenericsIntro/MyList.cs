db5ccfd36a1830d893607f3116b0fbd0eb9269b5
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-01-21 00:50:52 +0400

    Dördüncü ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> //T type demek. Kodu bana bir tip ver olarak okuyabiliriz.
+    {
+        T[] items;
+        //constructor: newlediğiniz anda çalışan classa denir.
+        public MyList()          // ctor and tab tab.
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length +1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        //Ürün Adı
+        public double UnitPrice { get; set; }
+        //Ürün Fiyatı
+        public int UnitInStock { get; set; }
+        //Ürün stok adedi.
+
+        //CRUD Create - read - update - delete
+
+    }
+}
